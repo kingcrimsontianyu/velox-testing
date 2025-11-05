4805c88aafefcb1c3fc5d856ce111adc12b9260d
Tianyu Liu <kingcrimsontianyu@gmail.com>
2025-11-05 16:17:49 -0500

    Update

--- velox/biu/generate_data.sh
+++ velox/biu/generate_data.sh
@@ -3,9 +3,17 @@
 python3 -m venv biu-venv-velox
 source biu-venv-velox/bin/activate
 
-data_script_dir=../../benchmark_data_tools
 scale_factor=0.01
-data_dest_dir=/mnt/nvme_ubuntu_test/velox-tpch-data/sf-${scale_factor}
+max_rows_per_file=200000
+
+# scale_factor=1
+# max_rows_per_file=200000
+
+data_script_dir=../../benchmark_data_tools
+data_dest_dir=/mnt/nvme_ubuntu_test/velox-tpch-data/biu-partition/sf-${scale_factor}
+
 
 pip install -r ${data_script_dir}/requirements.txt
-python ${data_script_dir}/generate_data_files.py -b tpch -d $data_dest_dir -s ${scale_factor} -c
+python ${data_script_dir}/generate_data_files.py \
+-b tpch -d $data_dest_dir -s ${scale_factor} -c \
+--max-rows-per-file ${max_rows_per_file}
